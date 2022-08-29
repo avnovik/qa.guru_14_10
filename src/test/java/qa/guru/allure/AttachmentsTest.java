1c0d3d7e30c8c00c496bda3be54bb9e761356f41
Novikov Aleksei <3900676@gmail.com>
2022-08-29 11:02:07 +0300

    added AttachmentsTest

---
+++ src/test/java/qa/guru/allure/AttachmentsTest.java
@@ -0,0 +1,35 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class AttachmentsTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+
+    @Test
+    public void testLambdaAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");
+            attachment("Sourse", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotatedAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.takeScreenshot();
+    }
+}
